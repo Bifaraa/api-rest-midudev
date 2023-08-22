549af3fb6a71d28db2c717b2b198787e13ed019d
bifara <jhon.1701720506@ucaldas.edu.co>
2023-08-21 22:04:19 -0500

    correcion con codigo de midudev

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,17 +1,28 @@
 const z = require('zod')
+
 const movieSchema = z.object({
   title: z.string({
-    required_error: 'title is required',
-    invalid_type_error: 'title must be a string'
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
   }),
-  year: z.number().int().positive().min(1900).max(2024),
+  year: z.number().int().min(1900).max(2024),
   director: z.string(),
   duration: z.number().int().positive(),
-  genre: z.array(z.enum(['action', 'comedy', 'drama', 'horror', 'romance']))
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  )
 })
 
-function validateMovie (objeto) {
-  return movieSchema.safeParse(objeto)
+function validateMovie (input) {
+  return movieSchema.safeParse(input)
 }
 
 function validatePartialMovie (input) {
